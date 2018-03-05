15729da9a75d74f17b24b1bbf892c2ddcfaf711a
TestLeafPages <homework@testleaf.com>
2018-03-05 12:48:52 +0530

    learn property

---
+++ LearnProperties.java
@@ -0,0 +1,18 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class LearnProperties {	
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		
+		Properties prop = new Properties();
+		prop.load(new FileInputStream("./sample.properties"));
+		System.out.println(prop.getProperty("URL"));
+		
+	}
+
+}
